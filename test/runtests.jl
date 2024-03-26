3cf50f95ad99a2e6549df209ebc4c5da932b3add
Olivier Vanvincq <olivier.vanvincq@univ-lille.fr>
2024-03-26 17:49:47 +0100

    Add files via upload

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
 using OpticalFibers
 using OpticalFibers.ModeSolvers
+using Gridap
 using Test
 
 @testset "Multistep" begin
@@ -16,8 +17,8 @@ using Test
 
 end
 
-@testset "ScalarFD" begin
-    modeFD=FD(0.6328,0,3,x->1.462420.-(1.462420+1.457420)*(x.>=8.335),10000,200,order=200);
+@testset "FEM1D" begin
+    modeFD=FEM1D(0.6328,0,x->(1.462420.-(1.462420+1.457420)*(x[1]>=8.335))^2,CartesianDiscreteModel((0,50),20000),neigs=3,order=3);
     neffFD=getproperty.(modeFD,:neff);
     UFD=2*pi*8.335/0.6328*sqrt.(1.462420^2 .-neffFD.^2);
     @test isapprox(UFD,[2.1845,4.9966,7.7642],atol=1E-4);
