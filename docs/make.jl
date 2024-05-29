2747879bb7da7208be4afb93c69a36fc338ec39d
Olivier Vanvincq <olivier.vanvincq@univ-lille.fr>
2024-05-29 17:20:37 +0200

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,7 +1,7 @@
 # see documentation at https://juliadocs.github.io/Documenter.jl/stable/
 push!(LOAD_PATH,"../src/")
 
-using Documenter, DocumenterCitations
+using Documenter, DocumenterCitations, OpticalFibers
 bib = CitationBibliography(joinpath(@__DIR__, "src", "refs.bib"); style=:numeric)
 
 ENV["GKSwstype"] = "100"
